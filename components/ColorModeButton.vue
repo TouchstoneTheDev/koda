55cb547be15f4763ed77c60678e1eeb86f601330
TouchstoneTheDev <tanmayveer.sits.classrooms@gmail.com>
2024-01-31 12:01:30 +0000

    basic

---
+++ components/ColorModeButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    class="flex items-center m-0"
+    :class="isMobileDevice ? 'navbar-item' : ''"
+    @click="toggleColorMode">
+    <ColorScheme placeholder="...">
+      <span v-if="isDarkMode">{{ $t('profileMenu.lightMode') }}</span>
+      <span v-else>{{ $t('profileMenu.darkMode') }}</span>
+      &nbsp;<NeoIcon icon="circle-half-stroke" />
+    </ColorScheme>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { NeoIcon } from '@kodadot1/brick'
+import { isMobileDevice } from '@/utils/extension'
+const { toggleColorMode, isDarkMode } = useTheme()
+</script>
---
+++ components/CookieBanner.vue
@@ -0,0 +1,12 @@
+<template>
+<Transition name="slide">
+<div v-if"!hasDisplayedCookieBanner" class="cookie-banner">
+<div 
+class=""
+</div>
+
+
+</Transition>
+
+
+</template>
